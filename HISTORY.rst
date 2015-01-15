4a4c4016a736882b5fc19bd54929c54df2826cae
blurrcat <blurrcat@gmail.com>
2015-01-15 11:55:40 +0800

    update version and history

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,5 +3,10 @@
 History
 -------
 
-0.1.0 ()
-++++++++++++++++++
+0.1.0
++++++
+Implements Flapjack events receiver
+
+0.2.0
++++++
+Implements Python API to Flapjack JSONAPI
--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@
 from setuptools import setup, find_packages
 
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 
-install_requires = []
+install_requires = [
+    'requests==2.5.1',
+]
 tests_require = install_requires + [
     'pytest==2.6.4', 'pytest-cov==1.6', 'mock==1.0.1']
 develop_require = tests_require + [
